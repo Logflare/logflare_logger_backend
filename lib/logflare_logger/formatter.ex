4d2c48cc2b38f0a94fb732d0607d8a378ba39b21
ontofractal <ontofractal@protonmail.com>
2019-04-26 23:06:22 +0300

    update formatter to convert metadata pid field to binary

--- lib/logflare_logger/formatter.ex
+++ lib/logflare_logger/formatter.ex
@@ -2,22 +2,32 @@ defmodule LogflareLogger.Formatter do
   alias Jason, as: JSON
 
   def format(level, message, ts, metadata) do
-    structured = %{
+    event = %{
       timestamp: Timex.to_unix(ts),
       level: level,
       message: message,
-      metadata: []
+      metadata: metadata
     }
 
     try do
-      JSON.encode!(structured)
+      event
+      |> metadata_to_binary()
+      |> JSON.encode!()
     rescue
-      _ ->
+      e ->
         %{
           timestamp: Timex.to_unix(ts),
           level: "error",
-          message: "Formatter encoding failed for message: #{message}"
+          message: "Formatter encoding failed for message: #{message} with error: #{inspect(e)}"
         }
     end
   end
+
+  def metadata_to_binary(%{metadata: %{pid: pid}} = event) when is_pid(pid) do
+    event
+    |> update_in([:metadata, :pid], &to_string(:erlang.pid_to_list(&1)))
+    |> metadata_to_binary()
+  end
+
+  def metadata_to_binary(s), do: s
 end
