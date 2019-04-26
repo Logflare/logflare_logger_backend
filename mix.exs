edd62e8ca75ac5b701189cea928c848816176b82
ontofractal <ontofractal@protonmail.com>
2019-04-26 17:38:18 +0300

    add jason, bypass and mix_test_watch dependencies

--- mix.exs
+++ mix.exs
@@ -22,8 +22,9 @@ defmodule LogflareLogger.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:mix_test_watch, ">= 0.9.0"},
+      {:jason, "~> 1.0"},
+      {:bypass, "~> 1.0", only: :test}
     ]
   end
 end
