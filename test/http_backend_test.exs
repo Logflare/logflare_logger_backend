4b50b2ecb27984a267336d04bc0a6fbfc4c755ae
ontofractal <ontofractal@protonmail.com>
2019-04-26 22:53:44 +0300

    add test case for correct conversion of metadata pid to binary

--- test/http_backend_test.exs
+++ test/http_backend_test.exs
@@ -1,5 +1,5 @@
 defmodule LogflareLogger.HttpBackendTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
   alias LogflareLogger.Formatter
   alias Jason, as: JSON
   require Logger
@@ -9,21 +9,23 @@ defmodule LogflareLogger.HttpBackendTest do
   @logger_backend {LogflareLogger.Backend, :test}
   Logger.add_backend(@logger_backend)
 
+  @default_config [
+    host: @host,
+    port: nil,
+    format: {Formatter, :format},
+    level: :info,
+    type: "testing",
+    metadata: []
+  ]
+
   setup do
     bypass = Bypass.open()
 
-    :ok =
-      Logger.configure_backend(
-        @logger_backend,
-        host: @host,
-        port: bypass.port,
-        format: {Formatter, :format},
-        level: :info,
-        type: "testing",
-        metadata: []
-      )
+    config = build_config(@default_config, port: bypass.port)
 
-    {:ok, bypass: bypass}
+    :ok = Logger.configure_backend(@logger_backend, config)
+
+    {:ok, bypass: bypass, config: config}
   end
 
   test "logger backend sends a POST request", %{bypass: bypass} do
@@ -43,13 +45,44 @@ defmodule LogflareLogger.HttpBackendTest do
     end)
 
     :ok = Logger.info(log_msg)
+
     Process.sleep(100)
   end
 
-  test "doesn't POST log events with a lower level", %{bypass: bypass} do
+  test "doesn't POST log events with a lower level", %{bypass: _bypass} do
     log_msg = "Incoming log from test"
 
     :ok = Logger.debug(log_msg)
     Process.sleep(100)
+  end
+
+  test "correctly handles metadata keys", %{bypass: bypass, config: config} do
+    Bypass.expect_once(bypass, "POST", "/api/v0/elixir-logger", fn conn ->
+      {:ok, body, conn} = Plug.Conn.read_body(conn)
+
+      assert %{
+               "level" => "info",
+               "message" => "Incoming log from test",
+               "metadata" => %{"pid" => pidbinary},
+               "timestamp" => _
+             } = JSON.decode!(body)
+
+      assert is_binary(pidbinary)
+
+      Plug.Conn.resp(conn, 200, "")
+    end)
+
+    log_msg = "Incoming log from test"
+
+    config = build_config(config, metadata: [:pid])
+
+    :ok = Logger.configure_backend(@logger_backend, config)
+
+    :ok = Logger.info(log_msg)
+    Process.sleep(100)
+  end
+
+  def build_config(config, opts) do
+    Keyword.merge(config, opts)
   end
 end
