b620ffe4e0c98bf2042d2684dd535fb7bf6846b5
ontofractal <ontofractal@protonmail.com>
2019-04-26 19:00:56 +0300

    update http backend test

--- test/http_backend_test.exs
+++ test/http_backend_test.exs
@@ -1,32 +1,37 @@
 defmodule LogflareLogger.HttpBackendTest do
   use ExUnit.Case, async: true
+  require Logger
 
   @host "127.0.0.1"
-  @port 42_314
 
-  @logger_backend {LogflareLoggerBackend, :test}
+  @logger_backend {LogflareLogger.Backend, :test}
   Logger.add_backend(@logger_backend)
 
   setup do
-    Logger.configure_backend(
-      @backend,
-      host: @host,
-      port: @port,
-      level: :info,
-      type: "testing",
-      metadata: []
-    )
+    bypass = Bypass.open()
 
-    bypass = Bypass.open(@port)
+    :ok =
+      Logger.configure_backend(
+        @logger_backend,
+        host: @host,
+        port: bypass.port,
+        level: :info,
+        type: "testing",
+        metadata: []
+      )
 
     {:ok, bypass: bypass}
   end
 
-  test "backend sends a POST request", %{bypass: bypass} do
+  test "logger backend sends a POST request", %{bypass: bypass} do
+    log_msg = "Incoming log from test"
+
     Bypass.expect_once(bypass, "POST", "/api/v0/elixir-logger", fn conn ->
+      {:ok, ^log_msg, conn} = Plug.Conn.read_body(conn)
       Plug.Conn.resp(conn, 200, "")
     end)
 
-    Logger.info("Incoming log from test")
+    :ok = Logger.info(log_msg)
+    Process.sleep(100)
   end
 end
