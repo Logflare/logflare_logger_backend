d808b811a501cd7b9a96deaaab48863710bab162
ontofractal <ontofractal@protonmail.com>
2019-04-29 18:41:00 +0300

    add Cache module

---
+++ lib/logflare_logger/cache.ex
@@ -0,0 +1,16 @@
+defmodule LogflareLogger.Cache do
+  @batch :batch
+  @cache __MODULE__
+
+  def add_event_to_batch(event, key \\ @batch) do
+    Cachex.get_and_update!(@cache, key, &[event | &1 || []])
+  end
+
+  def get_batch(key \\ @batch) do
+    Cachex.get!(@cache, key)
+  end
+
+  def reset_batch(key \\ @batch) do
+    Cachex.put!(@cache, key, [])
+  end
+end
