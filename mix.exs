823d76c9f0dff7776c1764232249b02526c511ed
ontofractal <ontofractal@protonmail.com>
2019-04-30 21:56:01 +0300

    rename package to logflare_logger_backend

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LogflareLogger.MixProject do
 
   def project do
     [
-      app: :logflare_logger,
+      app: :logflare_logger_backend,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
