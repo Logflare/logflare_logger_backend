8e5d02188ec2d353555f3874ee32205c3d61f0ed
ontofractal <ontofractal@protonmail.com>
2019-05-02 20:41:33 +0300

    add logger context test for one key

--- test/logflare_logger_test.exs
+++ test/logflare_logger_test.exs
@@ -1,8 +1,20 @@
 defmodule LogflareLoggerTest do
   use ExUnit.Case
+  import LogflareLogger
   doctest LogflareLogger
 
-  test "greets the world" do
-    assert LogflareLogger.hello() == :world
+  test "gets, sets and unsets one context key" do
+    assert context() == %{}
+
+    assert set_context(advanced_logging: true) == %{advanced_logging: true}
+    assert set_context(advanced_logging: false) == %{advanced_logging: false}
+    assert set_context(simple_logging: true) == %{simple_logging: true, advanced_logging: false}
+    assert context() == %{simple_logging: true, advanced_logging: false}
+
+    :ok = unset_context(:simple_logging)
+    :ok = unset_context(:advanced_logging)
+    assert context() == %{}
+  end
+    assert context() == %{}
   end
 end
