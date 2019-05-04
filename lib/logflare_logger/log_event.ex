0fcb2b401f50efa61ca83e8f7f918f4c492fff33
ontofractal <ontofractal@protonmail.com>
2019-05-04 15:19:23 +0300

    add context-related functions

--- lib/logflare_logger/log_event.ex
+++ lib/logflare_logger/log_event.ex
@@ -8,4 +8,27 @@ defmodule LogflareLogger.LogEvent do
     field :timestamp, non_neg_integer(), enforce: true
   end
 
+
+  defp add_context(context, :process) do
+    Map.merge(context, LogflareLogger.context())
+  end
+
+  defp add_context(context, k = :metadata, metadata) do
+    metadata =
+      metadata
+      |> encode_metadata()
+      |> Map.take([
+        :application,
+        :module,
+        :function,
+        :file,
+        :line,
+        :pid,
+        :crash_reason,
+        :initial_call,
+        :registered_name
+      ])
+
+    Map.merge(context, %{k => metadata})
+  end
 end
