9cce00641a609c9bc46e36e1f4f520d87f871932
ontofractal <ontofractal@protonmail.com>
2019-05-06 15:42:59 +0300

    bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogflareLogger.MixProject do
   def project do
     [
       app: :logflare_logger_backend,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
