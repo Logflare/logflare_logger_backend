8708613ebcbe50f26a0487774350814cc990db25
ontofractal <ontofractal@protonmail.com>
2019-05-06 15:40:30 +0300

    clean up formatter

--- lib/logflare_logger/formatter.ex
+++ lib/logflare_logger/formatter.ex
@@ -1,30 +1,18 @@
 defmodule LogflareLogger.Formatter do
-  def format(level, message, ts, metadata) do
-    event = %{
-      timestamp: Timex.to_unix(ts),
-      level: level,
-      message: message,
-      metadata: metadata
-    }
+  alias LogflareLogger.LogEvent
 
+  def format(level, message, ts, metadata) do
     try do
-      event
-      |> metadata_to_binary()
+      LogEvent.new(ts, level, message, metadata)
+      |> Map.from_struct()
     rescue
       e ->
         %{
           timestamp: Timex.to_unix(ts),
           level: "error",
-          message: "Formatter encoding failed for message: #{inspect message} with error: #{inspect(e)}"
+          message:
+            "Formatter encoding failed for message: #{inspect(message)} with error: #{inspect(e)}"
         }
     end
   end
-
-  def metadata_to_binary(%{metadata: %{pid: pid}} = event) when is_pid(pid) do
-    event.metadata.pid
-    |> update_in(&to_string(:erlang.pid_to_list(&1)))
-    |> metadata_to_binary()
-  end
-
-  def metadata_to_binary(event), do: event
 end
