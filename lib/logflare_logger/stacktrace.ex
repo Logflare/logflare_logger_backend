481c3a9765b29ddbbe1c13d654f6206f669c622b
ontofractal <ontofractal@protonmail.com>
2019-05-06 19:24:27 +0300

    implement formatting functions for Stacktrace entries

--- lib/logflare_logger/stacktrace.ex
+++ lib/logflare_logger/stacktrace.ex
@@ -7,14 +7,44 @@ defmodule LogflareLogger.Stacktrace do
     for i <- stacktrace, do: format_entry(i)
   end
 
-  defp format_entry({mod, fun, args, location}) do
+  defp format_entry({mod, fun, arity_or_args, location} = entry) do
     %{
-      module: field(:module, mod),
-      file: field(:file, location),
-      function: field(:function, fun),
-      args_count: field(:args_count, args),
-      line: field(:line, location)
+      module: format_field(:module, mod),
+      file: format_field(:file, location),
+      line: format_field(:line, location),
+      function: format_field(:function, fun, arity_or_args),
+      arity_or_args: format_field(:arity_or_args, arity_or_args)
     }
   end
 
+  defp format_field(_, ""), do: nil
+  defp format_field(_, nil), do: nil
+
+  defp format_field(field, term) when is_atom(term) do
+    format_field(field, to_string(term))
+  end
+
+  defp format_field(:module, mod) when is_binary(mod) do
+    String.replace_prefix(mod, "Elixir.", "")
+  end
+
+  defp format_field(field, []) when field in [:file, :line] do
+    nil
+  end
+
+  defp format_field(:file, location), do: Keyword.get(location, :file) |> to_string
+  defp format_field(:line, location), do: Keyword.get(location, :line)
+
+  defp format_field(:function, fun, args) do
+    arity = format_field(:arity_or_args, args)
+    "#{fun}/#{arity}"
+  end
+
+  defp format_field(:arity_or_args, arity) when is_integer(arity) do
+    arity
+  end
+
+  defp format_field(:arity_or_args, args) do
+    args
+  end
 end
