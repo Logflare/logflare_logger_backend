2e2f134ed75047d55ee94d3fbd1486cc64ad71a3
ontofractal <ontofractal@protonmail.com>
2019-05-15 18:00:01 +0300

    refactor LogEvent

--- lib/logflare_logger/log_event.ex
+++ lib/logflare_logger/log_event.ex
@@ -9,65 +9,63 @@ defmodule LogflareLogger.LogEvent do
     field :timestamp, non_neg_integer(), enforce: true
   end
 
-  def new(timestamp, level, message, metadata \\ %{}) do
+  def new(timestamp, level, message, %{crash_reason: cr} = metadata) when not is_nil(cr) do
+    message = message_to_string(message)
+
+    {_err, stacktrace} = cr
+
+    logflare_metadata =
+      %{}
+      |> add_context(:context, %{
+        pid: metadata.pid,
+        stacktrace: Stacktrace.format(stacktrace)
+      })
+
+    build_and_encode(message, timestamp, level, logflare_metadata)
+  end
+
+  def new(timestamp, level, message, metadata) do
     message = message_to_string(message)
 
     logflare_metadata =
-      case metadata[:crash_reason] do
-        {_err, stacktrace} ->
-          logflare_metadata =
-            %{}
-            |> add_context(:context, %{
-              pid: metadata.pid,
-              stacktrace: Stacktrace.format(stacktrace)
-            })
-            |> encode_metadata
+      %{}
+      |> add_context(:context, metadata)
+      |> add_context(:process, metadata)
 
-          logflare_metadata
+    build_and_encode(message, timestamp, level, logflare_metadata)
+  end
 
-        nil ->
-          logflare_metadata =
-            %{}
-            |> add_context(:context, metadata)
-            |> add_context(:process, metadata)
-
-          logflare_metadata
-      end
-
+  def build_and_encode(message, timestamp, level, metadata) do
     %__MODULE__{
       timestamp: timestamp,
       level: level,
       message: message,
-      metadata: logflare_metadata
+      metadata: metadata
     }
     |> encode_timestamp()
+    |> encode_metadata()
   end
 
   def message_to_string(message) when is_binary(message), do: message
-
   def message_to_string(message) when is_list(message) do
     strings =
       for m <- message do
-        if is_integer(m) do
-          to_string([m])
-        else
-          m
-        end
+        if is_integer(m), do: to_string([m]), else: m
       end
 
     Enum.join(strings)
   end
 
   defp add_context(logflare_metadata, :process, metadata) do
-    process = metadata |> Map.drop(Utils.default_metadata_keys())
+    default = Utils.default_metadata_keys()
+    process = Map.drop(metadata, default)
+
     Map.merge(logflare_metadata, process)
   end
 
   defp add_context(logflare_metadata, k = :context, metadata) do
-    metadata =
-      metadata
-      |> encode_metadata()
-      |> Map.take(Utils.default_metadata_keys() ++ [:stacktrace])
+    default = Utils.default_metadata_keys()
+    metadata = Map.take(metadata,  default ++ [:stacktrace])
 
     Map.merge(logflare_metadata, %{k => metadata})
   end
