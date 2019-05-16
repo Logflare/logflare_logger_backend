398f3326ae99ffdbd98b04b9ba0d02f17fb8ba9a
ontofractal <ontofractal@protonmail.com>
2019-05-16 20:30:42 +0300

    ApiClient improvements

--- lib/logflare_logger/api_client.ex
+++ lib/logflare_logger/api_client.ex
@@ -1,11 +1,13 @@
 defmodule LogflareLogger.ApiClient do
-  use Tesla
+  @moduledoc false
+  use Tesla, only: [:post], docs: false
+  @default_api_path "/logs/elixir/logger"
 
   @callback post_logs(Tesla.Client.t(), list(map), String.t()) ::
               {:ok, Tesla.Env.t()} | {:error, term}
 
-  def new(%{url: url, api_key: api_key}) do
-    [
+  def new(%{url: url, api_key: api_key}) when is_binary(url) and is_binary(api_key) do
+    middlewares = [
       Tesla.Middleware.FollowRedirects,
       {Tesla.Middleware.Headers,
        [
@@ -16,20 +18,24 @@ defmodule LogflareLogger.ApiClient do
       {Tesla.Middleware.BaseUrl, url},
       {Tesla.Middleware.Compression, format: "gzip"}
     ]
-    |> Tesla.client()
+
+    Tesla.client(middlewares)
   end
 
-  def post_logs(client, batch, source_id) when is_list(batch) do
+  def post_logs(%Tesla.Client{} = client, batch, source_id) when is_list(batch) do
     body =
       %{"batch" => batch, "source" => source_id}
+      # jsonify deeply converts all keywords to maps and all atoms to strings
+      # for Logflare server to be able to safely convert binary to terms
+      # using :erlang.binary_to_term(binary, [:safe])
       |> Iteraptor.jsonify(values: true)
       |> Map.update!("batch", &batch_to_payload/1)
       |> Bertex.encode()
 
-    Tesla.post(client, api_path(), body) 
+    Tesla.post(client, api_path(), body)
   end
 
-  def batch_to_payload(batch) do
+  def batch_to_payload(batch) when is_list(batch) do
     for log_entry <- batch do
       metadata =
         %{}
@@ -42,5 +48,5 @@ defmodule LogflareLogger.ApiClient do
     end
   end
 
-  def api_path, do: "/logs/elixir/logger"
+  def api_path, do: @default_api_path
 end
