966e51317adc1ef2322ad331d56a16ca53d43a70
ontofractal <ontofractal@protonmail.com>
2019-06-11 17:19:32 +0300

    simplify and refactor LogEvent

--- lib/logflare_logger/log_event.ex
+++ lib/logflare_logger/log_event.ex
@@ -5,74 +5,88 @@ defmodule LogflareLogger.LogEvent do
   alias LogflareLogger.{Stacktrace, Utils}
   @default_metadata_keys Utils.default_metadata_keys()
 
-  use TypedStruct
+  @doc """
+  Creates a LogEvent struct when all fields have serializable values
+  """
+  def new(timestamp, level, message, metadata) do
+    log =
+      %{
+        timestamp: timestamp,
+        level: level,
+        message: message,
+        metadata: metadata
+      }
+      |> encode_message()
+      |> encode_timestamp()
+      |> encode_metadata()
 
-  typedstruct do
-    field :level, atom, enforce: true
-    field :message, String.t(), enforce: true
-    field :context, map, default: %{}
-    field :timestamp, non_neg_integer(), enforce: true
+    {system_context, user_context} =
+      log.metadata
+      |> Map.split(@default_metadata_keys)
+
+    log
+    |> Map.drop([:metadata])
+    |> Map.put(:context, %{
+      system: system_context,
+      user: user_context
+    })
   end
 
   @doc """
-  Converts iodata message into binary.
+  Encodes message, if is iodata converts to binary.
   """
-  def new(ts, lvl, msg, meta) when is_list(msg) do
-    new(ts, lvl, message_to_string(msg), meta)
+  def encode_message(%{message: m} = log) do
+    %{log | message: to_string(m)}
   end
 
   @doc """
   Converts erlang datetime tuple into ISO:Extended binary.
   """
-  def new(ts, lvl, msg, meta) when is_tuple(ts) do
-    ts = encode_timestamp(ts)
-    new(ts, lvl, msg, meta)
+  def encode_timestamp(%{timestamp: t} = log) when is_tuple(t) do
+    timestamp =
+      t
+      # |> Timex.to_naive_datetime()
+      |> Timex.to_datetime(Timex.Timezone.local())
+      |> Timex.format!("{ISO:Extended}")
+
+    %{log | timestamp: timestamp}
+  end
+
+  def encode_metadata(%{metadata: meta} = log) do
+    meta =
+      meta
+      |> encode_pid()
+      |> encode_crash_reason()
+
+    %{log | metadata: meta}
   end
 
   @doc """
   Converts pid to string
   """
-  def new(ts, lvl, msg, %{pid: pid} = meta) when is_pid(pid) do
-    new(ts, lvl, msg, %{meta | pid: pid_to_string(pid)})
+  def encode_pid(%{pid: pid} = meta) when is_pid(pid) do
+    pid =
+      pid
+      |> :erlang.pid_to_list()
+      |> to_string()
+
+    %{meta | pid: pid}
   end
+
+  def encode_pid(meta), do: meta
 
   @doc """
   Adds formatted stacktrace to the metadata
   """
-  def new(ts, lvl, msg, %{crash_reason: cr} = meta) when not is_nil(cr) do
+  def encode_crash_reason(%{crash_reason: cr} = meta) when not is_nil(cr) do
     {_err, stacktrace} = cr
 
-    meta =
-      meta
-      |> Map.drop([:crash_reason])
-      |> Map.merge(%{stacktrace: Stacktrace.format(stacktrace)})
-
-    new(ts, lvl, msg, meta)
+    meta
+    |> Map.drop([:crash_reason])
+    |> Map.merge(%{stacktrace: Stacktrace.format(stacktrace)})
   end
 
-  @doc """
-  Creates a LogEvent struct when all fields have serializable values
-  """
-  def new(timestamp, level, message, metadata)
-      when is_binary(timestamp) and
-             is_binary(message) and
-             is_atom(level) and
-             is_map(metadata) do
-    {system_context, user_context} =
-      metadata
-      |> encode_metadata_charlists()
-      |> Map.split(@default_metadata_keys)
-
-    %__MODULE__{
-      timestamp: timestamp,
-      level: level,
-      message: message,
-      context: %{
-        system: system_context,
-        user: user_context
-      }
-    }
-  end
+  def encode_crash_reason(meta), do: meta
 
   def encode_metadata_charlists(metadata) do
     for {k, v} <- metadata, into: Map.new() do
@@ -85,29 +99,5 @@ defmodule LogflareLogger.LogEvent do
 
       {k, v}
     end
-  end
-
-  defp message_to_string(message) when is_binary(message), do: message
-
-  defp message_to_string(message) when is_list(message) do
-    strings =
-      for m <- message do
-        if is_integer(m), do: to_string([m]), else: m
-      end
-
-    Enum.join(strings)
-  end
-
-  defp pid_to_string(pid) when is_pid(pid) do
-    pid
-    |> :erlang.pid_to_list()
-    |> to_string()
-  end
-
-  defp encode_timestamp(ts) do
-    ts
-    |> Timex.to_naive_datetime()
-    |> Timex.to_datetime(Timex.Timezone.local())
-    |> Timex.format!("{ISO:Extended}")
   end
 end
