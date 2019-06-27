93c1801ca51e729ea9e1c40ae4956f986500f46c
ontofractal <ontofractal@protonmail.com>
2019-06-27 21:16:27 +0300

    update LogflareLogger.context fn signatures to match to Logger.metadata

--- lib/logflare_logger.ex
+++ lib/logflare_logger.ex
@@ -35,45 +35,49 @@ defmodule LogflareLogger do
   end
 
   @doc """
-  Merges map or keyword with the context in the process dictionary
+  If no argument is provided, returns the LogflareLogger context stored in the process dictionary.
   """
-  @spec merge_context(map() | keyword()) :: map()
-  def merge_context(map) when is_map(map) do
-    map
-    |> Keyword.new()
-    |> merge_context()
-  end
-
-  def merge_context(keyword) when is_list(keyword) do
-    Logger.metadata(keyword)
-    get_context()
-  end
-
-  @doc """
-  If key is passed, deletes context entry from the process dictionary. Otherwise deletes all context.
-  """
-  @spec delete_context() :: :ok
-  def delete_context() do
-    Logger.reset_metadata()
-  end
-
-  @spec delete_context(atom) :: :ok
-  def delete_context(key) do
-    Logger.metadata([{key, nil}])
-  end
-
-  @doc """
-  Get context in the process dictionary.
-  """
-  @spec get_context() :: map()
-  def get_context() do
+  @spec context() :: map()
+  def context() do
     Logger.metadata()
     |> Map.new()
   end
 
-  @spec get_context(atom) :: map()
-  def get_context(key) do
-    get_context()
+  @doc """
+  If the argument is an atom, returns LogflareLogger context for the given key.
+  If the argument is a map or keyword list, their keys/values are merged with the existing LogflareLogger context in the process dictionary. Setting the key to nil will remove that key from the context.
+  """
+  @spec context(map() | keyword()) :: map()
+  def context(map) when is_map(map) do
+    map
+    |> Keyword.new()
+    |> context()
+  end
+
+  def context(keyword) when is_list(keyword) do
+    Logger.metadata(keyword)
+    context()
+  end
+
+  @spec context(atom) :: map()
+  def context(key) when is_atom(key) do
+    context()
     |> Map.get(key)
+  end
+
+  @doc """
+  If no argument is passed, resets the whole context from the process dictionary.
+  If argument is an atom or a list of atoms, resets the context keeping only the given keys.
+  """
+  @spec reset_context() :: %{}
+  def reset_context() do
+    Logger.reset_metadata()
+    context()
+  end
+
+  @spec reset_context([atom, ...]) :: map()
+  def reset_context(keys) when is_list(keys) do
+    Logger.reset_metadata(keys)
+    context()
   end
 end
