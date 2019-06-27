e55a88cab540bb26ae86f79df47c09d40f04d80c
ontofractal <ontofractal@protonmail.com>
2019-06-27 21:11:16 +0300

    update LogflareLogger context tests

--- test/logflare_logger_test.exs
+++ test/logflare_logger_test.exs
@@ -20,7 +20,7 @@ defmodule LogflareLoggerTest do
     Application.put_env(:logflare_logger_backend, :max_batch_size, 2)
     {:ok, _pid} = Logger.add_backend(@logger_backend)
 
-    on_exit(&LogflareLogger.delete_context/0)
+    on_exit(&LogflareLogger.reset_context/0)
     :ok
   end
 
@@ -29,7 +29,7 @@ defmodule LogflareLoggerTest do
       allow ApiClient.new(any()), return: %Tesla.Client{}
       allow ApiClient.post_logs(any(), any(), any()), return: {:ok, %Tesla.Env{status: 200}}
 
-      LogflareLogger.merge_context(%{context_key: [:context_value, 1, "string"]})
+      LogflareLogger.context(%{context_key: [:context_value, 1, "string"]})
       Logger.bare_log(:info, "msg", data: %{a: 1})
       LogflareLogger.info("msg", data: %{a: 1})
 
@@ -48,45 +48,45 @@ defmodule LogflareLoggerTest do
 
   describe "Context" do
     test "gets, sets and unsets one context key" do
-      assert get_context() == %{}
+      assert context() == %{}
 
-      assert merge_context(advanced_logging: true) == %{advanced_logging: true}
-      assert merge_context(advanced_logging: false) == %{advanced_logging: false}
+      assert context(advanced_logging: true) == %{advanced_logging: true}
+      assert context(advanced_logging: false) == %{advanced_logging: false}
 
-      assert merge_context(simple_logging: true) == %{
+      assert context(simple_logging: true) == %{
                simple_logging: true,
                advanced_logging: false
              }
 
-      assert get_context() == %{simple_logging: true, advanced_logging: false}
+      assert context() == %{simple_logging: true, advanced_logging: false}
 
-      :ok = delete_context(:simple_logging)
-      :ok = delete_context(:advanced_logging)
-      assert get_context() == %{}
+      context(simple_logging: nil)
+      context(advanced_logging: nil)
+      assert context() == %{}
     end
 
     test "gets, sets and unsets multiple context keys" do
-      assert get_context() == %{}
+      assert context() == %{}
 
-      assert merge_context(key1: 1, key2: 2) == %{key1: 1, key2: 2}
-      assert merge_context(key2: 3, key4: 4) == %{key1: 1, key2: 3, key4: 4}
-      assert get_context() == %{key1: 1, key2: 3, key4: 4}
+      assert context(key1: 1, key2: 2) == %{key1: 1, key2: 2}
+      assert context(key2: 3, key4: 4) == %{key1: 1, key2: 3, key4: 4}
+      assert context() == %{key1: 1, key2: 3, key4: 4}
 
-      :ok = delete_context()
-      assert get_context() == %{}
+      reset_context()
+      assert context() == %{}
     end
 
     test "set context raises for invalid values" do
       assert_raise FunctionClauseError, fn ->
-        merge_context(nil)
+        context(11.11)
       end
 
       assert_raise FunctionClauseError, fn ->
-        merge_context(false)
+        context("false")
       end
 
       assert_raise FunctionClauseError, fn ->
-        merge_context(1_000)
+        context(1_000)
       end
     end
   end
