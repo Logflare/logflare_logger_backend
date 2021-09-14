bd9e360daf0cbbb6977714086a36f51d94b012db
chasers <chase@logflare.app>
2021-09-14 15:44:03 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogflareLogger.MixProject do
   def project do
     [
       app: :logflare_logger_backend,
-      version: "0.10.2",
+      version: "0.11.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
